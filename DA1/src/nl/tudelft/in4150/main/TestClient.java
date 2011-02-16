72d23a4eee30d59bc126a87183eea172e4ef0394
zandmans@gmail.com <zandmans@gmail.com@8dd802eb-b57e-c58b-4097-70724dac9592>
2011-02-16 18:41:09 +0000

    Added toString method to Message Changed the Algorithm in TestClient Added a new isDeliverable function to VectorClock and changed the key from String to Integer

--- DA1/src/nl/tudelft/in4150/main/TestClient.java
+++ DA1/src/nl/tudelft/in4150/main/TestClient.java
@@ -4,19 +4,22 @@
  */
 package nl.tudelft.in4150.main;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class TestClient extends RMIClient implements Runnable {
 	private boolean running; /* Running state of the text client; assigning false (while running) will cause clean termination of the thread. */
 	private List<Message> buffer;	/* The buffer where buffered message will be saved */
 	private VectorClock sendTime = new VectorClock();
+	private List<Message> delivered;
 
 	/** Create a new client */
 	public TestClient(int clientID) throws java.rmi.RemoteException {
 		super(clientID);
 
 		this.buffer = new ArrayList<Message>();
+		this.delivered = new ArrayList<Message>();
+
+		for (int i = 0; i < Config.CLIENT_COUNT; ++i) this.sendTime.put(i, 0); // init VectorClock
 
 		new Thread(this).start();
 		synchronized (this) {
@@ -34,50 +37,57 @@ public class TestClient extends RMIClient implements Runnable {
 		while (running) {
 			try { Thread.sleep(Config.CLIENT_DELAY[this.clientID]); } // Wait for some delay
 			catch (InterruptedException e) { e.printStackTrace(); }
-			Message message = new Message(this.clientID);
-			this.sendTime.incrementClock(Integer.toString(this.clientID));
-			message.sendTime = this.sendTime;
-			this.broadcastMessage(message); // Broadcast new message.
+			Message message = new Message(this.clientID);	// create m
+			this.sendTime.incrementClock(this.clientID); // V <- V + e_i
+			message.sendTime = this.sendTime;	// set V
+			this.broadcastMessage(message); // broadcast(m,V)
 		}
 	}
 
 	/** Broadcasts a message to all known clients, and outputs a notification. */
 	public void broadcastMessage(Message m) {
-		System.out.println("BROADCASTING FROM " + this.clientID + " TO CLIENTS 0-" + (Config.CLIENT_COUNT - 1) + " WITH MESSAGE ID " + m.messageID);
+		//System.out.println("==> BROADCASTING FROM " + this.clientID + " TO CLIENTS 0-" + (Config.CLIENT_COUNT - 1) + " WITH MESSAGE ID " + m.messageID);
 		for (int i = 0; i < Config.CLIENT_COUNT; i++)
-			this.sendMessage(m, i);
+			if(i != this.clientID) this.sendMessage(m, i); // only send to other clients
 	}
 
 	/** Show data when it is received. */
 	public void onMessageReceived(Message message) {
 		// TODO: Implement some reply mechanism here.
-		System.out.println("REC AT " + this.clientID + " MSGID " + message.messageID);
 
-		if (VectorClock.compare(this.sendTime, message.sendTime) != VectorComparison.SMALLER) {
-			this.sendTime = VectorClock.max(this.sendTime, message.sendTime);
-			// System.out.println(this.sendTime.toString());	// For testing purposes
+		if(this.clientID == 1) {
+			System.out.println("<== REC at CLIENT " + this.clientID + " MSG " + message.messageID + " FROM " + message.senderID);
+			System.out.println("VectorClock CLIENT " + message.senderID + " : " + message.sendTime.toString());
+			System.out.println("VectorClock RECEIVER : " + this.sendTime.toString());
+		}
+
+		if(VectorClock.isDeliverable(this.sendTime, message.sendTime, message.senderID)) {	// D_j(m)
 			this.deliver(message);
 			this.processBuffer();
-		} else
+		} else {
 			this.buffer.add(message);
+			if(this.clientID == 1) System.out.println("(++) MSG " + message.messageID + " added to BUFFER of CLIENT " + this.clientID);
+		}
 	}
 
 	/** Process the message */
 	public void deliver(Message message) {
 		// TODO: Implement a procedure done after receiving the right message
-		System.out.println("MSGID " + message.messageID + " processed by " + this.clientID);
+		this.delivered.add(message); // deliver(m)
+		this.sendTime.incrementClock(message.senderID); // V <- V + e_j
+		if(this.clientID == 1) System.out.println("(--) DELIVERED by CLIENT " + this.clientID + ": " + this.delivered.toString());
 	}
 
 	/** Check the buffer for messages which could be delivered */
 	public void processBuffer() {
-		// String strBuffer = "";	// For testing purposes
-		for (Message m : this.buffer) // Check all the messages in the buffer
-		{
-			// strBuffer += m.messageID + ", ";	// For testing purposes
-			if (VectorClock.compare(this.sendTime, m.sendTime) != VectorComparison.SMALLER) // The condition for delivering the message from the buffer
+		for(Iterator iterator = this.buffer.iterator(); iterator.hasNext();) {
+			Message m = (Message)iterator.next();
+			if (VectorClock.isDeliverable(this.sendTime, m.sendTime, m.senderID)) { // D_j(m)
 				deliver(m);
+				iterator.remove();
+			}
 		}
-		// System.out.println(strBuffer);	// For testing purposes
+		if(this.clientID == 1) System.out.println("(BF) BUFFER of CLIENT " + this.clientID + ": " + this.buffer.toString());	// For testing purposes
 	}
 
 	/** Create multiple clients, based on configuration */
