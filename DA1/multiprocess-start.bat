9680f67b1607b07835e7fe966bef468239ae1531
ndjong2 <ndjong2@8dd802eb-b57e-c58b-4097-70724dac9592>
2011-05-20 15:28:50 +0000

    Volledig herschreven versie. De gehele bijgewerkte oude versie is helaas verloren gegaan. Implementatie lijkt nu volledig te kloppen, alleen hij is nog niet synchroon genoeg, waardoor het misgaat bij ronde 3 en hoger (te veel overhead). Performance moet dus even verbeterd worden, inclusief synchronisatie. In volgende commit is code opgeruimd, hier is het opzettelijk nog beetje laten staan om evt terug te werken.

--- DA1/multiprocess-start.bat
+++ DA1/multiprocess-start.bat
@@ -1,6 +1,9 @@
-@start java -Djava.security.policy=my.policy -jar DA1.jar 10  3 localhost 4000 true true
+@start java -Djava.security.policy=my.policy -jar DA1.jar 0 localhost 7 2 1 4000 true
 timeout 2
-@start java -Djava.security.policy=my.policy -jar DA1.jar  3  6 localhost 2000 true false
-@start java -Djava.security.policy=my.policy -jar DA1.jar  6  4 localhost 2000 true false
-java -Djava.security.policy=my.policy -jar DA1.jar  4 10 localhost 2000 true false
+@start java -Djava.security.policy=my.policy -jar DA1.jar 1 localhost 7 2 0 2000 false
+@start java -Djava.security.policy=my.policy -jar DA1.jar 2 localhost 7 2 0 2000 false
+@start java -Djava.security.policy=my.policy -jar DA1.jar 3 localhost 7 2 0 2000 false
+@start java -Djava.security.policy=my.policy -jar DA1.jar 4 localhost 7 2 2 2000 false
+@start java -Djava.security.policy=my.policy -jar DA1.jar 5 localhost 7 2 0 2000 false
+java -Djava.security.policy=my.policy -jar DA1.jar 6 localhost 7 2 0 2000 false
 pause
--- DA1/src/nl/tudelft/in4150/ex3/DecisionTree.java
+++ DA1/src/nl/tudelft/in4150/ex3/DecisionTree.java
@@ -6,22 +6,32 @@ import java.util.ArrayList;
 public class DecisionTree {
 	public int node;	// Process ID
 	public int value; // Value received along path
+	public int level; // Current level
+	public DecisionTree parent;
 	public ArrayList<DecisionTree> children;
 
 	// Creates a new node.
 	public DecisionTree(int node, int value) {
+		this(node, value, 1, null);
+	}
+
+	// Creates a new node.
+	public DecisionTree(int node, int value, int level, DecisionTree parent) {
 		this.children = new ArrayList<DecisionTree>();
 		this.node = node;
 		this.value = value;
+		this.level = level;
+		this.parent = parent;
 	}
 
 	// Adds a new node at the right leaf of this tree, according to the path.
 	public void AddNewNode(LinkedList<Integer> pathIn, int value) {
 		LinkedList<Integer> path = new LinkedList<Integer>(pathIn); // Copy
-		DecisionTree newNode = new DecisionTree(path.removeLast(), value);
+		int node = path.removeLast();
 
 		int currentNode = path.removeFirst();
 		DecisionTree currentLeaf = this;
+		DecisionTree currentParent = null;
 		assert(this.node == currentNode);
 
 		while (!path.isEmpty()) {
@@ -32,7 +42,7 @@ public class DecisionTree {
 					break;
 				}
 		}
-		currentLeaf.children.add(newNode);
+		currentLeaf.children.add(new DecisionTree(node, value, pathIn.size(), currentLeaf));
 	}
 
 	// Recursive decide function
@@ -45,17 +55,17 @@ public class DecisionTree {
 	}
 
 	// Recursive function te complete tree to current level
+	/*
 	public static void HandleMissingMessages(Component caller, DecisionTree treeNode, LinkedList<Integer> currentPath, ArrayList<Integer> missingSenders) {
 		LinkedList<Integer> copyPath;
 		if (!treeNode.children.isEmpty()) { // This shouldn't be necessary, but just in case.
-			if (treeNode.children.get(0).children.isEmpty()) {
+			if (currentPath.size() == caller.currentRound) {
 				// Insert child to this node.
-				//HandleMissingMessages_(roundMessage, treeNode, currentPath)
 				for(Integer missingSender : missingSenders) {
-					if (Config.OUTPUT_DEBUGDATA > 0) System.out.println("I ("+caller.clientID+") have forged non-received message from "+missingSender);
-					treeNode.children.add(new DecisionTree(missingSender, Config.DEFAULT_VALUE));
+					if (Config.OUTPUT_DEBUGDATA > -1) System.out.println("I ("+caller.clientID+") have forged non-received message from "+missingSender);
 					copyPath = new LinkedList<Integer>(currentPath);
 					copyPath.add(missingSender);
+					treeNode.children.add(new DecisionTree(missingSender, Config.DEFAULT_VALUE, copyPath.size()-1, treeNode));
 					if (caller.currentRound < caller.maxFaults && !currentPath.contains(caller.clientID)) {
 						copyPath.add(caller.clientID);
 						caller.roundMessage.AddSubMsg(copyPath, Config.DEFAULT_VALUE);
@@ -70,9 +80,24 @@ public class DecisionTree {
 				}
 			}
 		} else {
-			if (Config.OUTPUT_DEBUGDATA > 0) System.out.println("Uhh, this is strange?");
+			if (Config.OUTPUT_DEBUGDATA > -1) {
+				String x;
+				x =(" -- Start afscheidsbrief --");
+				x+=("\nUhh, this is strange?");
+				x+=("\nI am "+caller.clientID);
+				x+=("\nMy current round is "+caller.currentRound);
+				x+=("\nMy root node is "+caller.data.node);
+				x+=("\nMy current tree node is "+treeNode.node);
+				x+=("\nMy current path in tree is "+currentPath);
+				x+=("\nAnd my current tree node doesn't seem to have children, which it is supposed to.");
+				x+=("\n -- Bye bye..... * slits throat * --");
+				x+=("\nPS (Post Suicide): my data tree was like.."+caller.data);
+				System.out.println(x);
+				System.exit(1);
+			}
 		}
 	}
+	*/
 	//public static void HandleMissingMessages_(Message roundMessage, DecisionTree treeNode, LinkedList<Integer> currentPath, ArrayList<Integer> missingSenders) {}
 
 	// Majority function
@@ -82,5 +107,20 @@ public class DecisionTree {
 		for (Integer i : a) count[i]++;
 		for (int i=1; i<10; i++) if (count[i] > count[currentMax]) currentMax = i;
 		return currentMax;
+	}
+
+	public String toString() {
+		String out = "Print tree:";
+		LinkedList<DecisionTree> Q = new LinkedList<DecisionTree>();
+		DecisionTree node;
+		Q.add(this);
+		while (!Q.isEmpty()) {
+			node = Q.removeFirst();
+			out += "\nNode @ depth="+node.level+" from parent="+(node.parent != null ? node.parent.node : "NONE")+" to me="+node.node+" with value="+node.value;
+			for (DecisionTree child : node.children) {
+				Q.add(child);
+			}
+		}
+		return(out);
 	}
 }
