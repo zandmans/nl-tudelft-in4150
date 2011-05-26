862090c3a997917474c98d138e4b388aca7b665e
ndjong2 <ndjong2@8dd802eb-b57e-c58b-4097-70724dac9592>
2011-05-26 11:40:39 +0000

    Bleggh, hier moet snel een eind aan komen nu!

--- trunk/DA1/multiprocess-start.bat
+++ trunk/DA1/multiprocess-start.bat
@@ -1,9 +1,9 @@
-@start java -Djava.security.policy=my.policy -jar DA1.jar 0 localhost 7 2 1 4000 true
+@start java -Djava.security.policy=my.policy -jar DA1.jar 0 localhost 7 2 0 4000 true
 timeout 2
-@start java -Djava.security.policy=my.policy -jar DA1.jar 1 localhost 7 2 0 2000 false
+@start java -Djava.security.policy=my.policy -jar DA1.jar 1 localhost 7 2 1 2000 false
 @start java -Djava.security.policy=my.policy -jar DA1.jar 2 localhost 7 2 0 2000 false
 @start java -Djava.security.policy=my.policy -jar DA1.jar 3 localhost 7 2 0 2000 false
 @start java -Djava.security.policy=my.policy -jar DA1.jar 4 localhost 7 2 2 2000 false
 @start java -Djava.security.policy=my.policy -jar DA1.jar 5 localhost 7 2 0 2000 false
-java -Djava.security.policy=my.policy -jar DA1.jar 6 localhost 7 2 0 2000 false
+@start java -Djava.security.policy=my.policy -jar DA1.jar 6 localhost 7 2 0 2000 false
 pause
